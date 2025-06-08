d7741840cad04cb5c1fe66e34c983f4cbbc1152a
batko1 <johnpetty097@gmail.com>
2025-06-09 00:25:58 +0300

    Add files via upload

---
+++ custom/n8n-nodes-telegram/nodes/index.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  nodes: [
+    {
+      name: 'Telegram Trigger',
+      type: 'n8n-nodes-base.telegramTrigger',
+      typeVersion: 1,
+      position: [0, 0],
+      parameters: {},
+    },
+  ],
+};
